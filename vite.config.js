d586f95e90ae37aac69ffc268c8554820bfb85ea
Oleksandr Adamenko <oleksandradamenko@yahoo.com>
2025-01-19 13:53:10 -0800

    hotfix(project): update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/OleksandrAdamenko/',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          return `assets/${assetInfo.name}-[hash][extname]`
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
-  }
+  
 })
